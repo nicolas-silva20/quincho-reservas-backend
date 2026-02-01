4ea38471d616ff54962421370642f67ee885bf5d
Silva Nicolas <nicolassilva1108@gmail.com>
2026-01-31 21:32:09 -0300

    fix: Agregar @Slf4j a ResenaController para logs de debug

--- src/main/java/com/elumbral/quincho/controller/ResenaController.java
+++ src/main/java/com/elumbral/quincho/controller/ResenaController.java
@@ -6,12 +6,14 @@ import com.elumbral.quincho.model.dto.ResenaRequestDTO;
 import com.elumbral.quincho.service.ResenaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/resenas")
 @RequiredArgsConstructor
